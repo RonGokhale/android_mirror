bd16f107a5f9c16c2657ef52fe10f30ebdb17b32
Ron <cips173@gmail.com>
2012-09-25 17:27:42 +0530

    Update gen_local_manifest.sh

--- gen_local_manifest.sh
+++ gen_local_manifest.sh
@@ -4,6 +4,6 @@ HEAD=$((`grep -n '<project ' local_manifest.xml.sample | head -n 1 | cut -d ':' 
 TAIL=$((`grep -n '<project ' local_manifest.xml.sample | tail -n 1 | cut -d ':' -f 1` + 1))
 
 head -n $HEAD local_manifest.xml.sample
-grep koush/proprietary_vendor_ default.xml |
-sed -e 's#name="koush/proprietary_vendor_\([^"]*\)"#path="vendor/\1" name="koush/proprietary_vendor_\1"#'
+grep  TheMuppets/proprietary_vendor_ default.xml |
+sed -e 's#name=" TheMuppets/proprietary_vendor_\([^"]*\)"#path="vendor/\1" name=" TheMuppets/proprietary_vendor_\1"#'
 tail -n +$TAIL local_manifest.xml.sample
